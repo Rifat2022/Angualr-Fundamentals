ba6a564c395fe81acf3e85af0c0af9ba83c3a055
Rifat2022 <md.rifathossainshaon@gmail.com>
2022-11-15 21:10:00 +0600

    upto creating first reactive form is done

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -14,4 +14,8 @@ export class AuthService{
     isAuthenticaed(){
         return !!this.currentUser;
     }
+    updateCurrentUser(firstName: string, lastName : string){
+        this.currentUser.firstName = firstName
+        this.currentUser.lastName = lastName
+    }
 }
